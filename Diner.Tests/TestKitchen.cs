58fdff8f5a41c9d41a6e4633f870d6eef2d3f820
Umair Rashid <umair@clamsandcrabs.com>
2015-04-23 22:04:14 -0500

    -Initial Check In

---
+++ Diner.Tests/TestKitchen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diner.Tests
+{
+    [TestClass]
+    public class TestKitchen
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TODO: Do it
+        }
+    }
+}
