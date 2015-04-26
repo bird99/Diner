a90faa6fc8db8f056c898ac2a62f193409c20362
Umair Rashid <umair@clamsandcrabs.com>
2015-04-26 14:48:52 -0500

    -Commit Test Files

---
+++ Diner.Tests/TestInputHandler.cs
@@ -0,0 +1,267 @@
+﻿using Diner.Core;
+using Diner.Core.Input;
+using Diner.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diner.Tests
+{
+    [TestClass]
+    public class TestInputHandler
+    {
+        #region Test: ParseTimeOfDay
+
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Invalid_String_Fails()
+        {
+            // Arrange
+            const string userInput = "brunch,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            Assert.IsTrue(timeOfDayResult.IsFailure);
+        }
+
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Morning_Succeeds()
+        {
+            // Arrange
+            const string userInput = "morning,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Morning);
+        }
+
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Night_Succeeds()
+        {
+            // Arrange
+            const string userInput = "night,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Night);
+        }        
+        
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Night_Is_Not_Case_Sensitive()
+        {
+            // Arrange
+            const string userInputNight = "NiGHT,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInputNight);
+
+            // Act
+            var timeOfDayResult = inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Night);
+        }
+
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Morning_Is_Not_Case_Sensitive()
+        {
+            // Arrange
+            const string userInputNight = "MoRNING,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInputNight);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Morning);
+        }      
+        
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Night_Trims_White_Space()
+        {
+            // Arrange
+            const string userInputNight = " night ,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInputNight);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Night);
+        }      
+        
+        [TestMethod]
+        public void Test_ParseTimeOfDay_For_Morning_Trims_White_Space()
+        {
+            // Arrange
+            const string userInputNight = " morning ,5,2,something,else";
+
+            var inputHandler = GetInputHandler(userInputNight);
+
+            // Act
+            var timeOfDayResult =inputHandler.ParseTimeOfDay();
+
+            // Assert
+            AssertTimeOfDayMatches(timeOfDayResult, TimeOfDay.Morning);
+        }
+
+        #endregion
+
+        #region Test: GetNextDishType
+
+        [TestMethod]
+        public void Test_GetNextDishType_For_Night_Succeeds()
+        {
+            // Arrange
+            const string userInput = "night,1,2,3,4";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var expectedEntreeResult = inputHandler.GetNextDishType();
+            var expectedSideResult = inputHandler.GetNextDishType();
+            var expectedDrinkResult = inputHandler.GetNextDishType();
+            var expectedDesertResult = inputHandler.GetNextDishType();
+
+            // Assert
+            AssertDishTypeMatches(expectedEntreeResult, DishType.Entree);
+            AssertDishTypeMatches(expectedSideResult, DishType.Side);
+            AssertDishTypeMatches(expectedDrinkResult, DishType.Drink);
+            AssertDishTypeMatches(expectedDesertResult, DishType.Desert);
+        }        
+        
+        [TestMethod]
+        public void Test_GetNextDishType_For_Morning_Succeeds()
+        {
+            // Arrange
+            const string userInput = "morning,1,2,3,4";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var expectedEntreeResult = inputHandler.GetNextDishType();
+            var expectedSideResult = inputHandler.GetNextDishType();
+            var expectedDrinkResult = inputHandler.GetNextDishType();
+            var expectedDesertResult = inputHandler.GetNextDishType();
+
+            // Assert
+            AssertDishTypeMatches(expectedEntreeResult, DishType.Entree);
+            AssertDishTypeMatches(expectedSideResult, DishType.Side);
+            AssertDishTypeMatches(expectedDrinkResult, DishType.Drink);
+            AssertDishTypeMatches(expectedDesertResult, DishType.Desert);
+        }
+
+        [TestMethod]
+        public void Test_GetNextDishType_For_Night_Trims_White_Space()
+        {
+            // Arrange
+            const string userInput = "night, 1,2 , 3 , 4";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var expectedEntreeResult = inputHandler.GetNextDishType();
+            var expectedSideResult = inputHandler.GetNextDishType();
+            var expectedDrinkResult = inputHandler.GetNextDishType();
+            var expectedDesertResult = inputHandler.GetNextDishType();
+
+            // Assert
+            AssertDishTypeMatches(expectedEntreeResult, DishType.Entree);
+            AssertDishTypeMatches(expectedSideResult, DishType.Side);
+            AssertDishTypeMatches(expectedDrinkResult, DishType.Drink);
+            AssertDishTypeMatches(expectedDesertResult, DishType.Desert);
+        }             
+        
+        [TestMethod]
+        public void Test_GetNextDishType_For_Morning_Trims_White_Space()
+        {
+            // Arrange
+            const string userInput = "morning, 1,2 , 3 , 4";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var expectedEntreeResult = inputHandler.GetNextDishType();
+            var expectedSideResult = inputHandler.GetNextDishType();
+            var expectedDrinkResult = inputHandler.GetNextDishType();
+            var expectedDesertResult = inputHandler.GetNextDishType();
+
+            // Assert
+            AssertDishTypeMatches(expectedEntreeResult, DishType.Entree);
+            AssertDishTypeMatches(expectedSideResult, DishType.Side);
+            AssertDishTypeMatches(expectedDrinkResult, DishType.Drink);
+            AssertDishTypeMatches(expectedDesertResult, DishType.Desert);
+        }
+
+        [TestMethod]
+        public void Test_GetNextDishType_Fails_On_Invalid_Item()
+        {
+            // Arrange
+            const string userInput = "night,1,2,3,4,6";
+
+            var inputHandler = GetInputHandler(userInput);
+
+            // Act
+            var expectedEntreeResult = inputHandler.GetNextDishType();
+            var expectedSideResult = inputHandler.GetNextDishType();
+            var expectedDrinkResult = inputHandler.GetNextDishType();
+            var expectedDesertResult = inputHandler.GetNextDishType();
+            var expectedFailureResult = inputHandler.GetNextDishType();
+
+            // Assert
+            AssertDishTypeMatches(expectedEntreeResult, DishType.Entree);
+            AssertDishTypeMatches(expectedSideResult, DishType.Side);
+            AssertDishTypeMatches(expectedDrinkResult, DishType.Drink);
+            AssertDishTypeMatches(expectedDesertResult, DishType.Desert);
+
+            Assert.IsTrue(expectedFailureResult.IsFailure, "Expected failure for menu item #6");
+        }
+
+        #endregion
+
+        #region Privates
+
+        /// <summary>
+        /// Asserts getNextDishTypeResult is successful and matched expectedDishType
+        /// </summary>
+        /// <param name="getNextDishTypeResult"></param>
+        /// <param name="expectedDishType"></param>
+        private void AssertDishTypeMatches(OperationResult<DishType> getNextDishTypeResult, DishType expectedDishType)
+        {
+            // Assert
+            Assert.IsTrue(getNextDishTypeResult.IsSuccessful);
+            Assert.AreEqual(expectedDishType, getNextDishTypeResult.Item);
+        }
+
+        /// <summary>
+        /// Asserts if parseTimeOfDayResult is successful and matches expected TimeOfDay
+        /// </summary>
+        /// <param name="parseTimeOfDayResult"></param>
+        /// <param name="expectedTimeOfDay"></param>
+        private void AssertTimeOfDayMatches(OperationResult<TimeOfDay> parseTimeOfDayResult, TimeOfDay expectedTimeOfDay)
+        {
+            // Assert
+            Assert.IsTrue(parseTimeOfDayResult.IsSuccessful);
+            Assert.AreEqual(expectedTimeOfDay, parseTimeOfDayResult.Item);
+        }
+
+        private InputHandler GetInputHandler(string userInput)
+        {
+            var inputHandler = new InputHandler(userInput);
+
+            return inputHandler;
+        }
+
+        #endregion
+    }
+}
---
+++ Diner.Tests/TestOrderModel.cs
@@ -0,0 +1,246 @@
+﻿using Diner.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diner.Tests
+{
+    [TestClass]
+    public class TestOrderModel
+    {
+        #region Test: GetOrderString
+
+        [TestMethod]
+        public void Test_GetOrderString_At_Night_Displays_Items_In_Correct_Order()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act
+            var addDesertResult = ordeModel.AddOrderItem(DishType.Desert);
+            var addSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var addEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+            var addDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+
+            //TODO: this could be more robust by using the actual MenuItems rather than hard coding the string for expeced value
+            const string expectedDisplayString = "Steak,Potato,Wine,Cake";
+
+            Assert.IsTrue(addEntreeResult.IsSuccessful);
+            Assert.IsTrue(addSideResult.IsSuccessful);
+            Assert.IsTrue(addDrinkResult.IsSuccessful);
+            Assert.IsTrue(addDesertResult.IsSuccessful);
+            Assert.AreEqual(expectedDisplayString, ordeModel.GetOrderString());
+        }
+
+        [TestMethod]
+        public void Test_GetOrderString_At_Night_Handles_Multiple_Sides()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act
+            var addDesertResult = ordeModel.AddOrderItem(DishType.Desert);
+            var addSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var addSideSecondResult = ordeModel.AddOrderItem(DishType.Side);
+            var addEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+            var addDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+
+            //TODO: this could be more robust by using the actual MenuItems rather than hard coding the string for expeced value
+            const string expectedDisplayString = "Steak,Potato(x2),Wine,Cake";
+
+            Assert.IsTrue(addEntreeResult.IsSuccessful);
+            Assert.IsTrue(addSideResult.IsSuccessful);
+            Assert.IsTrue(addSideSecondResult.IsSuccessful);
+            Assert.IsTrue(addDrinkResult.IsSuccessful);
+            Assert.IsTrue(addDesertResult.IsSuccessful);
+            Assert.AreEqual(expectedDisplayString, ordeModel.GetOrderString());
+        }
+        
+        [TestMethod]
+        public void Test_GetOrderString_At_Mornning_Displays_Items_In_Correct_Order()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act
+            var addSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var addEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+            var addDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+
+            //TODO: this could be more robust by using the actual MenuItems rather than hard coding the string for expeced value
+            const string expectedDisplayString = "Eggs,Toast,Coffee";
+
+            Assert.IsTrue(addEntreeResult.IsSuccessful);
+            Assert.IsTrue(addSideResult.IsSuccessful);
+            Assert.IsTrue(addDrinkResult.IsSuccessful);
+            Assert.AreEqual(expectedDisplayString, ordeModel.GetOrderString());
+        }
+
+        [TestMethod]
+        public void Test_GetOrderString_At_Morning_Handles_Multiple_Drinks()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act
+            var addSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var addEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+            var addDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+            var addSecondDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+
+            //TODO: this could be more robust by using the actual MenuItems rather than hard coding the string for expeced value
+            const string expectedDisplayString = "Eggs,Toast,Coffee(x2)";
+
+            Assert.IsTrue(addEntreeResult.IsSuccessful);
+            Assert.IsTrue(addSideResult.IsSuccessful);
+            Assert.IsTrue(addDrinkResult.IsSuccessful);
+            Assert.AreEqual(expectedDisplayString, ordeModel.GetOrderString());
+        }
+
+        #endregion
+
+        #region Test: AddOrderItem
+
+        [TestMethod]
+        public void Test_AddOrderItem_At_Night_Allows_Max_One_Entree()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act & Assert
+            TestAddingMultipleEntrees(ordeModel);
+        }        
+        
+        [TestMethod]
+        public void Test_AddOrderItem_At_Morning_Allows_Max_One_Entree()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act & Assert
+            TestAddingMultipleEntrees(ordeModel);
+        }
+
+        [TestMethod]
+        public void Test_AddOrderItem_At_Night_Allows_Multiple_Sides()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act
+            var firstAddSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var secondAddSideResult = ordeModel.AddOrderItem(DishType.Side);
+
+            // Assert
+            Assert.IsTrue(firstAddSideResult.IsSuccessful);
+            Assert.IsTrue(secondAddSideResult.IsSuccessful);
+        }
+
+        [TestMethod]
+        public void Test_AddOrderItem_At_Morning_Resitricts_Multiple_Sides()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act
+            var firstAddSideResult = ordeModel.AddOrderItem(DishType.Side);
+            var secondAddSideResult = ordeModel.AddOrderItem(DishType.Side);
+
+            // Assert
+            Assert.IsTrue(firstAddSideResult.IsSuccessful);
+            Assert.IsTrue(secondAddSideResult.IsFailure);
+        }
+
+        [TestMethod]
+        public void Test_AddOrderItem_At_Night_Allows_Max_One_Drink()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act
+            var firstAddDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+            var secondAddDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+            Assert.IsTrue(firstAddDrinkResult.IsSuccessful);
+            Assert.IsTrue(secondAddDrinkResult.IsFailure);
+        }        
+        
+        [TestMethod]
+        public void Test_AddOrderItem_At_Morning_Allows_Multiple_Drinks()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act
+            var firstAddDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+            var secondAddDrinkResult = ordeModel.AddOrderItem(DishType.Drink);
+
+            // Assert
+            Assert.IsTrue(firstAddDrinkResult.IsSuccessful);
+            Assert.IsTrue(secondAddDrinkResult.IsSuccessful);
+        }
+
+        [TestMethod]
+        public void Test_AddOrderItem_At_Night_Allows_Max_One_Desert()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Night);
+
+            // Act
+            var firstAddDesertResult = ordeModel.AddOrderItem(DishType.Desert);
+            var secondAddDesertResult = ordeModel.AddOrderItem(DishType.Desert);
+
+            // Assert
+            Assert.IsTrue(firstAddDesertResult.IsSuccessful);
+            Assert.IsTrue(secondAddDesertResult.IsFailure);
+        }             
+        
+        [TestMethod]
+        public void Test_AddOrderItem_At_Morning_Has_No_Desert()
+        {
+            // Arrange
+            var ordeModel = GetOrderModel(TimeOfDay.Morning);
+
+            // Act
+            var addDesertResult = ordeModel.AddOrderItem(DishType.Desert);
+
+            // Assert
+            Assert.IsTrue(addDesertResult.IsFailure);
+        }
+
+        #endregion
+
+        #region Privates
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ordeModel"></param>
+        private static void TestAddingMultipleEntrees(OrderModel ordeModel)
+        {
+            // Act
+            var firstEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+            var secondEntreeResult = ordeModel.AddOrderItem(DishType.Entree);
+
+            // Assert
+            Assert.IsTrue(firstEntreeResult.IsSuccessful);
+            Assert.IsTrue(secondEntreeResult.IsFailure);
+        }
+
+        private OrderModel GetOrderModel(TimeOfDay timeOfDay)
+        {
+            var orderModel = new OrderModel(timeOfDay);
+
+            return orderModel;
+        }
+
+        #endregion
+    }
+}
